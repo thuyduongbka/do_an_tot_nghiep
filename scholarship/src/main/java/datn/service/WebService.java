8611453ba28fccf68eb78eaff81c5056196161c1
thuyduongbka <7ngayconang@gmail.com>
2020-10-30 13:46:06 +0700

    commit call api crawler

--- scholarship/src/main/java/datn/service/WebService.java
+++ scholarship/src/main/java/datn/service/WebService.java
@@ -1,10 +1,12 @@
 package datn.service;
 
 import datn.base.BaseService;
+import datn.crawler.CrawlerService;
 import datn.entity.WebEntity;
 import datn.repository.WebRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +15,15 @@ import java.util.List;
 public class WebService extends BaseService<WebEntity, WebRepository> {
     private static Logger logger = LoggerFactory.getLogger(WebService.class);
     
+    @Autowired
+    private CrawlerService crawlerService;
+    
     public List<WebEntity> getAll(){
         return repository.findAll();
+    }
+    
+    public void crawler(){
+        crawlerService.crawlerApi("1");
     }
     
 }
