ffc4f0ef52254ac85ce18f02cc30e64d262ad001
thuyduongbka <7ngayconang@gmail.com>
2020-11-25 22:51:12 +0700

    first commit

---
+++ recommend_system/information.py
@@ -0,0 +1,15 @@
+class Infomation():
+
+    def __init__(self, country, school, najor, level, money, time):
+
+        
+        pass
+
+    def get_vector(self):
+        vector = []
+        try:
+            pass
+        except:
+            pass 
+
+        return vector
