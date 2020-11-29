32e3a048f79af4a41e5f177037f4706ba4e7cc6e
thuyduongbka <7ngayconang@gmail.com>
2020-11-29 23:23:14 +0700

    Fix crawler trường school

--- scholarship_crawling/scholarship_crawling/spiders/SqlScholarshipPosition.py
+++ scholarship_crawling/scholarship_crawling/spiders/SqlScholarshipPosition.py
@@ -50,13 +50,17 @@ class SqlScholarshipPositionSpider(scrapy.Spider):
             item["urlImage"] = response.xpath("//figure/img/@src").get()
             time = response.xpath("//p[contains(strong/text(),'Application Deadline')]/text()").get()
             item["time"] = datetime.strptime(time.replace(':', '').strip(), '%B %d, %Y').date()
-            school = response.xpath("//li[contains(strong/text(),'University or Organization')]/text()").get()
+            school = response.xpath("//li[contains(strong/text(),'University or Organization')]/a/text()").get()
+            if school is None:
+                school = response.xpath("//li[contains(strong/text(),'University or Organization')]/text()").get()
             item["school"] = school.replace(':', '').strip();
 
             major = response.xpath("//li[contains(strong/text(),'Departmen')]/text()").get()
             item["major"] = getListMajor(major.replace(':', '').strip())
 
             level = response.xpath("//li[contains(strong/text(),'Course Level')]/a/text()").get()
+            if level is None:
+                level = response.xpath("//li[contains(strong/text(),'Course Level')]/a/strong/text()").get()
             if level is None:
                 level = response.xpath("//li[contains(strong/text(),'Course Level')]/text()").get()
             item["level"] = getListLevel(re.split(',|and ', level.replace(':', '').strip()))
